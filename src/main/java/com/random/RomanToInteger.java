4bf006bf350ef3348ddeb9e024de0012167159c2
Saurabh Prakash <saurabhprakash@Saurabhs-MacBook-Air.local>
2025-10-02 14:57:56 +0100

    adding changes to random package

--- src/main/java/com/random/RomanToInteger.java
+++ src/main/java/com/random/RomanToInteger.java
@@ -15,33 +15,28 @@ public class RomanToInteger {
     public int romanToDecimal(String s) {
         // code here
         HashMap<Character, Integer> map = new HashMap<>();
-        map.put('I',1);
-        map.put('V',5);
-        map.put('X',10);
-        map.put('L',50);
-        map.put('C',100);
-        map.put('D',500);
-        map.put('M',1000);
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
 
 
-        int val =0;
-        for(int i=0;i<s.length();){
+        int val = 0;
+
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            int curr = map.get(c);
-
-            if(i+1 < s.length()){
-                char c2 = s.charAt(i+1);
-                int next = map.get(c2);
-                if(curr >= next){
-                    val = val + curr;
-                    i = i +1;
-                }else{
-                    val = val + (next - curr);
-                    i = i+2;
+            if (i + 1 < s.length()) {
+                char d = s.charAt(i + 1);
+                if (map.get(c) < map.get(d)) {
+                    val = val - map.get(c);
+                } else {
+                    val = val + map.get(c);
                 }
-            }else{
-                val = val + curr;
-                i= i+1;
+            } else {
+                val = val + map.get(c);
             }
         }
 
