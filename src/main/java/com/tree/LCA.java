e802f4c3d62b544d146ac6d1317fdf00c9884dc5
Saurabh Prakash <saurabhprakash@Saurabhs-MacBook-Air.local>
2025-10-09 12:14:03 +0100

    fixing tree code

--- src/main/java/com/tree/LCA.java
+++ src/main/java/com/tree/LCA.java
@@ -14,71 +14,56 @@ public class LCA {
         lca(root, 5, 4);
     }
 
-   static Node lca(Node root, int n1, int n2) {
-        // Your code here
+    static Node lca(Node root, int n1, int n2) {
 
         ArrayList<Node> l1 = new ArrayList<Node>();
         ArrayList<Node> l2 = new ArrayList<Node>();
 
-        // System.out.println("value1 ");
-
         createList(root, n1, l1);
-        // System.out.println("value2 ");
-       flag = true;
+        flag = true;
         createList(root, n2, l2);
 
-      /*  for(int i=0;i < l1.size();i++){
-            System.out.println(l1.get(i).data);
+        Node prev1 = null;
+        if (l1.size() < l2.size()) {
+            for (int i = 0; i < l1.size(); i++) {
+                if (l1.get(i).data == l2.get(i).data) {
+                    prev1 = l1.get(i);
+                } else {
+                    //  prev1= l1.get(i-1);
+                    break;
+                }
+            }
+        } else {
+            for (int i = 0; i < l2.size(); i++) {
+                if (l1.get(i).data == l2.get(i).data) {
+                    prev1 = l1.get(i);
+                    //  continue;
+                } else {
+                    // prev1= l2.get(i-1);
+                    break;
+                }
+            }
         }
-
-        System.out.println("value ");
-
-        for(int i=0;i < l2.size();i++){
-            System.out.println(l2.get(i).data);
-        }*/
-
-
-       Node prev1=null;
-       if(l1.size()< l2.size()){
-           for(int i=0;i<l1.size();i++){
-               if(l1.get(i).data == l2.get(i).data){
-                   prev1= l1.get(i);
-               }else{
-                 //  prev1= l1.get(i-1);
-                   break;
-               }
-           }
-       }else{
-           for(int i=0;i<l2.size();i++){
-               if(l1.get(i).data == l2.get(i).data){
-                   prev1= l1.get(i);
-                 //  continue;
-               }else{
-                  // prev1= l2.get(i-1);
-                   break;
-               }
-           }
-       }
         return prev1;
     }
 
-  static   boolean flag = true;
-  static   void createList(Node root, int val, ArrayList<Node> list){
-        if(root == null){
+    static boolean flag = true;
+
+    static void createList(Node root, int val, ArrayList<Node> list) {
+        if (root == null) {
             return;
         }
 
         list.add(root);
 
-        if(root.data == val){
+        if (root.data == val) {
             flag = false;
             return;
         }
 
-
-            createList(root.left, val, list);
-            createList(root.right, val, list);
-      if(flag) {
+        createList(root.left, val, list);
+        createList(root.right, val, list);
+        if (flag) {
             list.remove(root);
         }
     }
