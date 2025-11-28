7ab05c585080fe7699910de1b935b46f35771bf4
Saurabh Prakash <saurabhprakash@Saurabhs-MacBook-Air.local>
2025-11-28 11:51:58 +0000

    adding code

--- src/main/java/com/dynamicProgramming/StairCaseProbOrderDoesNotMatter.java
+++ src/main/java/com/dynamicProgramming/StairCaseProbOrderDoesNotMatter.java
@@ -14,59 +14,90 @@ public class StairCaseProbOrderDoesNotMatter {
 
     Long countWays(int n) {
         // your code here
-        int[] arr = {1,2};
+        int[] arr = {1, 2};
+        //  return find(n, arr, 0);
 
-        // return find(n, arr, 0);
+        Long[][] dp = new Long[n + 1][arr.length];
 
-        Long[][] dp = new Long[n+1][2];
-        for(int i=0;i< n+1;i++){
+        for (int i = 0; i < n + 1; i++) {
             Arrays.fill(dp[i], -1L);
         }
 
-        return findMem(n, arr, 0, dp);
+        //  return findMem(n, arr, 0, dp);
+
+        return findTab(n, arr, 0);
+
 
     }
 
-    Long find(int n, int[] arr, int ind){
-        if(n ==0){
+    Long find(int n, int[] arr, int ind) {
+        if (n == 0) {
             return 1L;
         }
 
-        if(n < 0){
+        if (n < 0) {
             return 0L;
         }
 
-        if(ind >= arr.length){
+        if (ind == arr.length) {
             return 0L;
         }
 
-        Long inc = find(n-arr[ind], arr, ind);
-        Long exc = find(n, arr, ind+1);
+        Long inc = find(n - arr[ind], arr, ind);
+        Long exc = find(n, arr, ind + 1);
 
         return inc + exc;
+
+
     }
 
 
-    Long findMem(int n, int[] arr, int ind, Long[][] dp){
-        if(n ==0){
+    Long findMem(int n, int[] arr, int ind, Long[][] dp) {
+        if (n == 0) {
             return 1L;
         }
 
-        if(n < 0){
+        if (n < 0) {
             return 0L;
         }
 
-        if(ind >= arr.length){
+        if (ind == arr.length) {
             return 0L;
         }
 
-        if(dp[n][ind] !=-1){
+        if (dp[n][ind] != -1) {
             return dp[n][ind];
         }
 
-        Long inc = findMem(n-arr[ind], arr, ind, dp);
-        Long exc = findMem(n, arr, ind+1, dp);
+        Long inc = findMem(n - arr[ind], arr, ind, dp);
+        Long exc = findMem(n, arr, ind + 1, dp);
 
         return dp[n][ind] = inc + exc;
+    }
+
+
+    Long findTab(int n, int[] arr, int ind) {
+
+        Long[][] dp = new Long[n + 1][arr.length + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            Arrays.fill(dp[i], 0L);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            dp[0][i] = 1L;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = arr.length - 1; j >= 0; j--) {
+                Long inc = 0L;
+                if (i - arr[j] >= 0)
+                    inc = dp[i - arr[j]][j];
+                Long exc = dp[i][j + 1];
+
+                dp[i][j] = inc + exc;
+            }
+        }
+        return dp[n][0];
     }
 }
