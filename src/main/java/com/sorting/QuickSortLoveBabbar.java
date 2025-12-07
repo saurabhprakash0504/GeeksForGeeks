7e57c1b3bc4cf3c26dd99b0dd93893d5c7935ada
Saurabh Prakash <saurabhprakash@Saurabhs-MacBook-Air.local>
2025-12-07 11:34:50 +0000

    adding quicksort

--- src/main/java/com/sorting/QuickSortLoveBabbar.java
+++ src/main/java/com/sorting/QuickSortLoveBabbar.java
@@ -4,30 +4,29 @@ public class QuickSortLoveBabbar {
 
     public static void main(String[] args) {
 
-        int[] arr= {10, 7, 8, 9, 1, 5};
-        QuickSortLoveBabbar qs= new QuickSortLoveBabbar();
-        qs.quickSort(arr, 0, arr.length-1);
-        for(int num: arr){
+        int[] arr = {10, 7, 8, 9, 1, 5};
+        QuickSortLoveBabbar qs = new QuickSortLoveBabbar();
+        qs.quickSort(arr, 0, arr.length - 1);
+        for (int num : arr) {
             System.out.print(num + " ");
         }
-
     }
 
     public void quickSort(int[] arr, int s, int e) {
-        if(s >=e){
-            return ;
+        if (s >= e) {
+            return;
         }
-        int newPivotIndex = partition(arr, s, e);
+        int pivotIndex = partition(arr, s, e);
 
-        quickSort(arr, s, newPivotIndex-1);
-        quickSort(arr, newPivotIndex+1, e);
+        quickSort(arr, s, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, e);
     }
 
     private int partition(int[] arr, int s, int e) {
-        int pivot = arr[s];
-        int noOfSmallerElement =0;
-        for(int i=s+1;i<=e;i++){
-            if(arr[i]<= pivot){
+        int pivotValue = arr[s];
+        int noOfSmallerElement = 0;
+        for (int i = s + 1; i <= e; i++) {
+            if (arr[i] <= pivotValue) {
                 noOfSmallerElement++;
             }
         }
@@ -36,32 +35,30 @@ public class QuickSortLoveBabbar {
 
         swap(arr, s, newPivotIndex);
 
-        int i=s;
-        int j= e;
-        while(i <newPivotIndex && j > newPivotIndex){
+        int i = s;
+        int j = e;
+        while (i < newPivotIndex && j > newPivotIndex) {
 
-            while(arr[i] <= arr[newPivotIndex]){
+            while (arr[i] <= pivotValue) {
                 i++;
             }
 
-            while(arr[j] > arr[newPivotIndex]){
+            while (arr[j] > pivotValue) {
                 j--;
             }
 
-            if(i <newPivotIndex && j > newPivotIndex)
+            if (i < newPivotIndex && j > newPivotIndex) {
                 swap(arr, i, j);
-            i++;
-            j--;
-
+                i++;
+                j--;
+            }
         }
-
         return newPivotIndex;
-
     }
 
-    void swap(int[] arr, int s, int newPivotIndex){
-        int temp= arr[s];
+    void swap(int[] arr, int s, int newPivotIndex) {
+        int temp = arr[s];
         arr[s] = arr[newPivotIndex];
-        arr[newPivotIndex]= temp;
+        arr[newPivotIndex] = temp;
     }
 }
