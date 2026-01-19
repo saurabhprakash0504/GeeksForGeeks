518e8c3975f019f03d58f233df4ad06e69105a38
Saurabh Prakash <saurabhprakash@Saurabhs-MacBook-Air.local>
2026-01-19 15:07:07 +0000

    adding string

--- src/main/java/com/tree/PreOrderTraversal.java
+++ src/main/java/com/tree/PreOrderTraversal.java
@@ -1,5 +1,8 @@
 package com.tree;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class PreOrderTraversal {
     public static void main(String[] args) {
         Node tree = new Node(10);
@@ -11,7 +14,14 @@ public class PreOrderTraversal {
         tree.right = new Node(30);
         tree.right.right = new Node(60);
 
-        preOrderTraversal(tree);
+        //preOrderTraversal(tree);
+
+        ArrayList<Integer> al = new ArrayList<>();
+
+        findIterative(tree, al);
+
+        System.out.println(al);
+
     }
 
     static void preOrderTraversal(Node root){
@@ -21,5 +31,21 @@ public class PreOrderTraversal {
         System.out.println(root.data);
         preOrderTraversal(root.left);
         preOrderTraversal(root.right);
+    }
+
+    static void findIterative(Node root, ArrayList<Integer> al){
+
+        Stack<Node> stack = new Stack<Node>();
+
+        Node curr = root;
+        while(curr != null || !stack.isEmpty() ){
+            while(curr != null){
+                stack.push(curr);
+                al.add(curr.data);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            curr = curr.right;
+        }
     }
 }
