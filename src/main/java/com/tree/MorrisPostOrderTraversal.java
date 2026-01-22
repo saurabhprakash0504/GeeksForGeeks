850f919f90b5d9d37c115c725706432706f70426
Saurabh Prakash <saurabhprakash@Saurabhs-MacBook-Air.local>
2026-01-22 17:05:06 +0000

    adding changes for trees

---
+++ src/main/java/com/tree/MorrisPostOrderTraversal.java
@@ -0,0 +1,62 @@
+package com.tree;
+
+import java.util.ArrayList;
+
+public class MorrisPostOrderTraversal {
+
+    public static void main(String[] args) {
+
+        Node tree = new Node(10);
+        tree.left = new Node(20);
+        tree.left.left = new Node(40);
+        tree.left.right = new Node(50);
+        tree.left.right.left = new Node(70);
+        tree.left.right.right = new Node(80);
+        tree.right = new Node(30);
+        tree.right.right = new Node(60);
+
+        ArrayList<Integer> al = new ArrayList<>();
+
+        MorrisPostOrderTraversal obj = new MorrisPostOrderTraversal();
+        obj.morrisTraversal(tree, al);
+
+        System.out.println(al);
+
+    }
+
+    void morrisTraversal(Node root, ArrayList<Integer> al){
+
+        Node curr = root;
+        while(curr != null){
+
+            //IF LEFT IS NOT NULL
+            if(curr.left != null){
+                Node temp = curr.left;
+                while(temp != null &&  temp.right != null && temp.right != curr){
+
+                    temp = temp.right;
+
+                }
+                //TO CREATE LINK && ADD CURR VALUE
+                if( temp.right == null){
+                    al.add(curr.data);
+                    temp.right = curr;
+                    curr = curr.left;
+                }
+                //TO REMOVE LINK
+                else {
+                    temp.right = null;
+                    curr = curr.right;
+                }
+
+            }
+            //IF LEFT IS NULL
+            else{
+                al.add(curr.data);
+                curr = curr.right;
+            }
+
+        }
+
+    }
+}
